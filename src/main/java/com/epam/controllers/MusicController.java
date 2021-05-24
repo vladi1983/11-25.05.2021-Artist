4ee721489eceb4bd2281ca5364170fcb885dd784
jeka1978 <kit2009>
2021-05-24 11:51:32 +0300

    music lab almost solved

--- src/main/java/com/epam/controllers/MusicController.java
+++ src/main/java/com/epam/controllers/MusicController.java
@@ -1,18 +1,40 @@
 package com.epam.controllers;
 
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Stream;
 
 /**
  * @author Evgeny Borisov
  */
+@RestController
 public class MusicController {
 
-/*
-    @GetMapping
-    public List<String> topX(String pathToData, int x) {
+    @Autowired
+    private WordService wordService;
+
+    @Autowired
+    private JavaSparkContext sc;
+
+    @GetMapping("music/top/")
+    public List<String> topX(/*String pathToData, int x*/) {
+        JavaRDD<String> rdd = sc.parallelize(List.of("Java","Python", "Java", "Java", "Scala", "Scala"));
+        Iterator<String> wordsIterator = wordService.topX(rdd.rdd(), 2).iterator();
+        List<String> words = new ArrayList<>();
+        while (wordsIterator.hasNext()) {
+            String word = wordsIterator.next();
+            words.add(word);
+        }
+
+        return words;
         //todo finish it
-    }*/
+    }
 }
